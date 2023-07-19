4e2c0badc01b64a65031bf3cfc580c2ddbc7df7e
srihari-m-s <srihari.magadi@gmail.com>
2023-07-19 16:20:52 +0530

    Updating changes for deployment

--- pages/index.js
+++ pages/index.js
@@ -2,6 +2,8 @@ import Head from 'next/head';
 import Layout, { siteTitle } from '../components/layout';
 import utilStyles from '../styles/utils.module.css';
 import { getSortedPostsData } from '../lib/posts';
+import Link from 'next/link';
+import Date from '../components/date';
 
 export async function getStaticProps() {
   const allPostsData = getSortedPostsData();
@@ -23,21 +25,23 @@ export default function Home({ allPostsData }) {
       <section className={utilStyles.headingMd}>
         <p className={utilStyles.paddingBlock}>Hello! I am Srihari, an aspiring web developer.</p>
         <p className={utilStyles.paddingBlock}>
-          (This is a sample website - you’ll be building a site like this on{' '}
-          <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
+          (This is a sample website - you can build a site like this on the{' '}
+          <a href="https://nextjs.org/learn">Next.js tutorial</a>.)
         </p>
       </section>
       
       <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
+
         <h2 className={utilStyles.headingLg}>Blog</h2>
+
         <ul className={utilStyles.list}>
           {allPostsData.map(({ id, date, title }) => (
             <li className={utilStyles.listItem} key={id}>
-              {title}
+              <Link href={`/posts/${id}`}>{title}</Link>
               <br />
-              {id}
-              <br />
-              {date}
+              <small className={utilStyles.lightText}>
+                <Date dateString={date} />
+              </small>
             </li>
           ))}
         </ul>
--- posts/ssg-ssr.md
+++ posts/ssg-ssr.md
@@ -7,10 +7,10 @@ We recommend using **Static Generation** (with and without data) whenever possib
 
 You can use Static Generation for many types of pages, including:
 
-- Marketing pages
-- Blog posts
-- E-commerce product listings
-- Help and documentation
+1. Marketing pages
+2. Blog posts
+3. E-commerce product listings
+4. Help and documentation
 
 You should ask yourself: "Can I pre-render this page **ahead** of a user's request?" If the answer is yes, then you should choose Static Generation.
 
